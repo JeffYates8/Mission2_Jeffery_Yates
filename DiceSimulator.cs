bf28ae9c9eac27057ad1020c31c8e425ddfa054e
Jeffery YaTES <jefferybyates@gmail.com>
2025-01-15 19:44:58 -0700

    Add project files.

---
+++ DiceSimulator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission1_Jeffery_Yates
+{
+    public class DiceSimulator
+    {
+
+        private Random random = new Random();
+        public int[] SimulateRolls(int numOfRolls)
+        {
+            int[] rollResults = new int[13];
+
+            for (int rollCounter = 0; rollCounter < numOfRolls; rollCounter++) 
+            {
+                int firstRoll = random.Next(1, 7);
+                int secondRoll = random.Next(1, 7);
+                int rollSum = firstRoll + secondRoll;
+
+                rollResults[rollSum]++;
+            }
+        
+            return rollResults;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Mission1_Jeffery_Yates;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        // user input for num of rolls
+        int numOfRolls = int.Parse(Console.ReadLine());
+
+        //
+        DiceSimulator simulator = new DiceSimulator();
+        int[] rollResults = simulator.SimulateRolls(numOfRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {numOfRolls}.\n");
+
+        //for loop to determine roll sum with number of rolls
+        for (int total = 2; total <= 12; total++)
+        {
+            int percentage = (rollResults[total] * 100) / numOfRolls;
+            Console.WriteLine($"{total}: {new string('*', percentage)}");
+        }
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+    }
+}
